1da4118b67a0edd376b874ded97586c8aee7c479
Augusto Rost <augustorost93@gmail.com>
2024-03-11 19:28:22 -0300

    validaçao

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,18 @@ import express from 'express'
 import cors from 'cors'
 import tweetroutes from './routes/tweet.routes'
 import userRoutes from '../src/routes/user.routes'
+import likeRoutes from '../src/routes/like.routes'
+import authRoutes from '../src/routes/auth.routes'
+
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
 app.use(userRoutes)
 app.use(tweetroutes)
-
+app.use(likeRoutes)
+app.use(authRoutes)
 
 
 app.listen(3333, ()=>{
--- src/routes/tweet.routes.ts
+++ src/routes/tweet.routes.ts
@@ -1,23 +1,24 @@
 import express from "express"
 import { TweetController } from "../controllers/tweet.controller"
+import { validateToken } from "../middlewares/auth.middleware";
 
 const router = express.Router();
 
 const tweetcontroller = new TweetController()
 
 //lista todos tweets
-router.get('/tweets', tweetcontroller.index)
+router.get('/:idUser/tweets', validateToken, tweetcontroller.index)
 
  // criar novo tweets
-router.post('/tweets', tweetcontroller.store)
+router.post('/:idUser/tweets', validateToken, tweetcontroller.store)
 
 // //lista um unico tweets
-router.get('/tweets/:id', tweetcontroller.show)
+router.get('/:idUser/tweets/:id', validateToken, tweetcontroller.show)
 
 // //atualiza tweets
-router.put('/tweets/:id', tweetcontroller.update)
+router.put('/:idUser/tweets/:id', validateToken, tweetcontroller.update)
 
 // //deleta tweets
-router.delete('/tweets/:id', tweetcontroller.delete)
+router.delete('/:idUser/tweets/:id', validateToken, tweetcontroller.delete)
 
 export default router;
